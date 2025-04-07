837910d919ecfb6ee8b75240e9f4ddee492ebac1
Stephen Kerr <kerrstephen17@gmail.com>
2025-04-08 00:49:41 +0100

    added the analysis file and the data to the project repo

---
+++ analysis.py
@@ -0,0 +1,6 @@
+# This program is the central analysis file for the Programming and Scripting Module.
+# It imports the Iris Data and perfoms the analysis on it.
+# Author: Stephen Kerr
+
+# imports 
+
